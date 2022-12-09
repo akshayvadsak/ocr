cb563a5ad9d0cbe2e85130cdcf2a5ca300f704aa
Akshay <akshay.mountgames@gmail.com>
2022-12-09 14:01:04 +0530

    Init OCR

---
+++ index.js
@@ -0,0 +1,9 @@
+// const Tesseract = require("tesseract.js");
+
+// Tesseract.recognize(
+//   'https://sharelatex-wiki-cdn-671420.c.cdn77.org/learn-scripts/images/5/5f/Ragged2eOLV21.png',
+//   'eng',
+//   { logger: m => console.log(m) }
+// ).then(({ data: { text } }) => {
+//   console.log(text);
+// })
